f249ba19b82fce083cd5e40a96594cbf5537a16b
Jesús E. Cabrera Rosales <jesuscabrerarosales47@gmail.com>
2025-06-16 12:02:13 -0500

    practice using ls package

--- ls.js
+++ ls.js
@@ -1,4 +1,5 @@
-const fs = require('node:fs/promises')
+const fs = require('node:fs/promises');
+const path = require('node:path');
 
 fs.readdir('.', (err, files) => {
     if(err){
@@ -9,3 +10,38 @@ fs.readdir('.', (err, files) => {
         console.log(files)
     })
 })
+
+const folder = process.argv[2] ?? '.'
+
+async function ls (folder) {
+    let files
+    try {
+        files = await fs.readdir(folder)
+    } catch {
+        console.error('no se pudo leer el directorio ${folder}')
+        process.exit(1)
+    }
+
+    const filesPromises = files.map(async file =>{
+        const filePath = path.join(folder,file)
+        let stats
+        try {
+            stats = await fs.stat(filePath)
+        } catch {
+            console.error('no se pudo leer el archivo ${filePath}')
+            process.exit(1)
+        }
+
+        const isDirectory = stats.isDirectory()
+        const fileType = isDirectory ? 'd' : '-'
+        const fileSize = stats.size
+        const fileModified = stats.mtime.toLocaleString()
+
+        return  `${fileType} ${file} ${fileSize.toString()} ${fileModified} `
+    })
+
+    const filesInfo = await Promise.all(filesPromises)
+    filesInfo.forEach(filesInfo => console.log(filesInfo))
+}
+
+ls(folder)
